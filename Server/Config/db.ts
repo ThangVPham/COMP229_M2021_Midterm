a60b373fa48bf1de29d49d2adb0491430b22ad32
ThangVPham <thangpham1412@gmail.com>
2021-06-21 17:06:47 -0400

    first commit

---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost/books_store";
+export const RemoteURI = "your remote host information goes here"
+export const HostName = "LocalHost";
+export const Secret = "someSecret";
